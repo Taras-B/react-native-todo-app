700fd84d38b72fb4ddc0d6e00fe28472235e7f15
Taras-B <taras5475@gmail.com>
2020-07-22 14:05:41 +0300

    create navbar

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { Navbar } from './src/components/Navbar'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Navbar />
+      <Text>Open up app! -'Taras</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +19,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+export const Navbar = () => {
+  return (
+    <View>
+      <Text>Todo App</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
